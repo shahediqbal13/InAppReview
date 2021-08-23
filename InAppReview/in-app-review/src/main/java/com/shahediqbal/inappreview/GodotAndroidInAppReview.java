63d0f6daf6daeddba1f6807a2bcf51d4b9f7b6f6
Shahed Iqbal <shahediqbal13@gmail.com>
2021-08-24 01:04:07 +0600

    in app review first implementation

---
+++ InAppReview/in-app-review/src/main/java/com/shahediqbal/inappreview/GodotAndroidInAppReview.java
@@ -0,0 +1,82 @@
+package com.shahediqbal.inappreview;
+
+import android.app.Activity;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.ChecksSdkIntAtLeast;
+import androidx.annotation.NonNull;
+import androidx.collection.ArraySet;
+
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.review.model.ReviewErrorCode;
+import com.google.android.play.core.review.testing.FakeReviewManager;
+import com.google.android.play.core.tasks.Task;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+import org.godotengine.godot.plugin.UsedByGodot;
+
+import java.util.Set;
+
+public class GodotAndroidInAppReview extends GodotPlugin {
+    private static final String TAG = "godot";
+    private static final String PLUGIN_NAME = "GodotAndroidInAppReview";
+
+    private final Activity activity;
+
+    public GodotAndroidInAppReview(Godot godot) {
+        super(godot);
+        activity = getActivity();
+    }
+
+    @NonNull
+    @Override
+    public String getPluginName() {
+        return PLUGIN_NAME;
+    }
+
+    @NonNull
+    @Override
+    public Set<SignalInfo> getPluginSignals() {
+        Set<SignalInfo> signals = new ArraySet<>();
+        signals.add(new SignalInfo(Signals.IN_APP_REVIEW_FAILED_TO_SHOW, String.class));
+        signals.add(new SignalInfo(Signals.IN_APP_REVIEW_FLOW_COMPLETED));
+        return signals;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.LOLLIPOP)
+    @UsedByGodot
+    public boolean isInAppReviewSupportedOs() {
+        return android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    public void ShowInAppReview(boolean isFake) {
+        ReviewManager manager = isFake ? new FakeReviewManager(activity) : ReviewManagerFactory.create(activity);
+
+        Task<ReviewInfo> request = manager.requestReviewFlow();
+        request.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // We can get the ReviewInfo object
+                ReviewInfo reviewInfo = task.getResult();
+                Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+                flow.addOnCompleteListener(task2 -> {
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                    Log.i(TAG, PLUGIN_NAME + ": Review flow completed");
+                    emitSignal(Signals.IN_APP_REVIEW_FLOW_COMPLETED);
+                });
+
+            } else {
+                // There was some problem, log or handle the error code.
+                String errorMessage = task.getException().getMessage();
+                Log.e(TAG, PLUGIN_NAME + ": " + errorMessage);
+                emitSignal(Signals.IN_APP_REVIEW_FAILED_TO_SHOW, errorMessage);
+            }
+        });
+    }
+}
---
+++ InAppReview/in-app-review/src/main/java/com/shahediqbal/inappreview/Signals.java
@@ -0,0 +1,6 @@
+package com.shahediqbal.inappreview;
+
+public class Signals {
+    public static final String IN_APP_REVIEW_FAILED_TO_SHOW = "on_in_app_review_failed_to_show";
+    public static final String IN_APP_REVIEW_FLOW_COMPLETED = "on_in_app_review_flow_completed";
+}
